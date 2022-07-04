aa45c49ff38c7cd5e93aaa63ff8efdc223091985
hima1207 <65057793+hima1207@users.noreply.github.com>
2022-07-04 00:08:24 -0400

    Create Select All.sql

---
+++ SQL/Select All.sql
@@ -0,0 +1,6 @@
+-- Select All
+-- Problem : Query all columns (attributes) for every row in the CITY table.
+-- Link : https://www.hackerrank.com/challenges/select-all-sql/problem
+-- ORACLE solution is as follows:
+
+SELECT * FROM city;
